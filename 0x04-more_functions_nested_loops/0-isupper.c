f58718559bcba1e7478a2b6e104546518b516d85
Rasheeed-git <Harunarrasheeed@gmail.com>
2022-09-15 15:48:22 -0700

    Task-0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,7 +4,9 @@
  *   * @c: the character to be checked
  *    * Return: 1 if c is uppercase, 0 otherwise
  *     */
-int _isupper(int c)
-{
-		return (c >= 'A' && c <= 'Z');
+int _isupper(int c){
+	if ((c >= 'A') && (c <= 'Z')){
+		return(1);
+	}
+	return (0);
 }
