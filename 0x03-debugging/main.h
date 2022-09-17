d1ba0640062ce06cc02c32de7f86d4fca8cc4b83
Rasheeed-git <Harunarrasheeed@gmail.com>
2022-09-16 17:38:39 -0700

    Task-3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-void print_remaining_days(int , int, int)  
-int convert_day(int month, int day);
+void print_remaining_days(int, int, int)  
+int convert_day(int, int);
 int largest_number(int a, int b, int c);
 #endif /* MAIN_H */
