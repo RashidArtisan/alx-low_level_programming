8b59252f797f9310ce3376f722367d820390ac52
Rasheeed-git <Harunarrasheeed@gmail.com>
2022-09-21 14:15:58 -0700

    Tasks

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,21 @@
-#include "main.h"
+nclude "main.h"
 
 /**
- * print_number - prints an integer;
- * @n: integer to be printed;
- */
-void print_number(int n)
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
 {
-	unsigned int n1;
-
-	if (n < 0)
-	{
-		n1 = -n;
-		_putchar('-');
-	} else
-	{
-		n1 = n;
-	}
-
-	if (n1 / 10)
-		print_number(n1 / 10);
-
-	_putchar((n1 % 10) + '0');
+	    print_number(98);
+	        _putchar('\n');
+		    print_number(402);
+		        _putchar('\n');
+			    print_number(1024);
+			        _putchar('\n');
+				    print_number(0);
+				        _putchar('\n');
+					    print_number(-98);
+					        _putchar('\n');
+						    return (0);
 }
