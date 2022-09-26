66b12a6cf61c5ebde29bfb0d7502c1c9fecb189f
Rasheeed-git <Harunarrasheeed@gmail.com>
2022-09-26 15:27:27 -0700

    header correction

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
 int _putchar(char c);
 
-void *_memset(char *s, char b, unsigned int n);
+char*_memset(char *s, char b, unsigned int n);
 
-void *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 char *_strchr(char *s, char c);
 
