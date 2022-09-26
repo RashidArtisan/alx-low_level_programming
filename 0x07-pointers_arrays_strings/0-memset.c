82cebafac17e788a40387bc2e94308dac380191d
Rasheeed-git <Harunarrasheeed@gmail.com>
2022-09-26 14:25:17 -0700

    Task-1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
-
 /**
- *  * _memset - function
- *   * @s : the address of the memory we want to filled it u
- *    * @b : the caracter that will fill the memory
- *     * @n : the number of times we will fill the memory
- *      * Return: memory.
- *       */
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
-	while (i < n)
+	unsigned int i;
+
+	for (i = 0; n > 0; i++, n--)
 	{
 		s[i] = b;
-		i++;
 	}
+
 	return (s);
 }
