39c3d4148e465db6a323ec57f186e82dcffb80e2
Rasheeed-git <Harunarrasheeed@gmail.com>
2022-10-06 13:05:52 -0700

    Task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,9 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 int find_len(char *str);
 char *create_xarray(int size);
 char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
+void get_prod(char *prod, char *mul, int num1, int num2);
 void add_nums(char *final_prod, char *next_prod, int next_len);
 
 /**
@@ -90,19 +88,19 @@ int get_digit(char c)
 /**
  * get_prod - Multiplies a string of numbers by a single digit.
  * @prod: The buffer to store the result.
- * @mult: The string of numbers.
- * @digit: The single digit.
- * @zeroes: The necessary number of leading zeroes.
+ * @mul: The string of numbers.
+ * @num1: The single digit.
+ * @num2: The necessary number of leading zeroes.
  *
- * Description: If mult contains a non-digit, the function
+ * Description: If mul contains a non-digit, the function
  *              exits with a status value of 98.
  */
-void get_prod(char *prod, char *mult, int digit, int zeroes)
+void get_prod(char *prod, char *mul, int num1, int num2)
 {
-	int mult_len, num, tens = 0;
+	int mul_len, num, tens = 0;
 
-	mult_len = find_len(mult) - 1;
-	mult += mult_len;
+	mul_len = find_len(mul) - 1;
+	mul += mul_len;
 
 	while (*prod)
 	{
@@ -112,21 +110,21 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 
 	prod--;
 
-	while (zeroes--)
+	while (num2--)
 	{
 		*prod = '0';
 		prod--;
 	}
 
-	for (; mult_len >= 0; mult_len--, mult--, prod--)
+	for (; mul_len >= 0; mul_len--, mul--, prod--)
 	{
-		if (*mult < '0' || *mult > '9')
+		if (*mul < '0' || *mul > '9')
 		{
 			printf("Error\n");
 			exit(98);
 		}
 
-		num = (*mult - '0') * digit;
+		num = (*mul - '0') * num1;
 		num += tens;
 		*prod = (num % 10) + '0';
 		tens = num / 10;
